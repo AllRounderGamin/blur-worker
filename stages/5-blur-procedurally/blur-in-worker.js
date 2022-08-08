bbbce195e5c1844e518f4a79c34210d2033858c1
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-08-08 23:58:26 +0100

    add 5-blur-procedurally

---
+++ stages/5-blur-procedurally/blur-in-worker.js
@@ -0,0 +1,57 @@
+let worker;
+
+createWorker();
+
+function createWorker() {
+  worker = new Worker('worker.js', { type: 'module' });
+  worker.addEventListener('message', handleMessage);
+}
+
+const responseReceivers = new Map();
+let nextMessageId = 0;
+
+function handleMessage(e) {
+  const { type, id } = e.data;
+  const { receiver, showBlurProgress, reportProgress } = responseReceivers.get(id);
+
+  if (type === 'done') {
+    console.log('finished job', id);
+    receiver(e.data.result);
+    responseReceivers.delete(id);
+  } else if (type === 'progress' && reportProgress) {
+    showBlurProgress(e.data.current);
+    reportProgress(e.data.progress);
+  }
+}
+
+export function blurImageData(imageData, n = 3, reportProgress, showBlurProgress) {
+  const message = {
+    imageData,
+    n,
+    id: nextMessageId,
+  };
+  nextMessageId += 1;
+
+  console.log('submitting job', message.id);
+  worker.postMessage(message);
+
+  return new Promise((resolve, reject) => {
+    responseReceivers.set(message.id, { receiver: resolve, reject, reportProgress, showBlurProgress });
+  });
+}
+
+export function terminateWorker() {
+  // stop ongoing work
+  worker.terminate();
+
+  // notify all callers that their jobs are cancelled
+  const cancelled = new Error('blur cancelled');
+  for (const [id, responseReceiver] of responseReceivers.entries()) {
+    responseReceiver.reject(cancelled);
+    console.log('cancelled job', id);
+  }
+  responseReceivers.clear();
+
+  // create a new worker ready for jobs
+  createWorker();
+}
